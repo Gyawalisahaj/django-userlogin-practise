292d86676104b21c19ddf4ef5384904957a33534
Gyawalisahaj <sahajgnawali@gmail.com>
2025-07-08 11:58:38 +0545

    add files amd handle reg and login system

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, HttpResponse,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
+def HomePage(request):
+    return render(request, 'home.html')
+
+
+def SignupPage(request):
+    if request.method == 'POST':
+        name = request.POST.get('fullname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        dob = request.POST.get('dob')
+        gender = request.POST.get('gender')
+
+        
+        print("Name:", name)
+        print("Email:", email)
+        print("Password:", password)
+        print("DOB:", dob)
+        print("Gender:", gender)
+        my_user = User.objects.create_user(username=name, email=email, password=password)
+        my_user.save()
+        return redirect('loginpage')
+
+    return render(request, 'signup.html')
+
+def LoginPage(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user =  authenticate(request,email = email, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('homepage')
+
+        else:
+            return HttpResponse('Wrong username or password !!!')
+
+    return render(request, 'login.html')
--- registration/urls.py
+++ registration/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1 import views
+from app1.views import SignupPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.SignupPage, name = 'signuppage'),
+    path('login/', views.LoginPage, name = 'loginpage'),
+    path('home/', views.HomePage, name = 'homepage' )
 ]
